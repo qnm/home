d66ee9af80069fdbf5a432e9ab58513113eb98ed
Rob Sharp <rob@sharp.id.au>
2023-09-30 10:45:57 +1000

    fleek: apply

--- pop-os/custom.nix
+++ pop-os/custom.nix
@@ -1,20 +1,4 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-
-  # home.packages = with pkgs; [
-  #   # linux only
-  #   firefox
-  #   flatpak
-  #   gnome.gnome-tweaks
-  #   gnome.gnome-keyring
-  #   gnome.gnome-screenshot
-  #   gnome.nautilus
-  #   gnome.gnome-shell-extensions
-  #   gnomeExtensions.appindicator
-  #   gnomeExtensions.pop-shell
-  #   rocminfo
-  #   hipcc
-  #   tidal-hifi
-  #   shortwave
-  # ]);
+  fonts.fontconfig.enable = true;
 }
