fab3922de34c2dd39cb95f344cacd53898e79b23
Rob Sharp <qnm@macbook.local>
2023-10-05 21:28:07 +1100

    join new system

--- flake.nix
+++ flake.nix
@@ -20,6 +20,8 @@
 
   outputs = { self, nixpkgs, home-manager, fleek, ... }@inputs: {
     
+     packages.aarch64-darwin.fleek = fleek.packages.aarch64-darwin.default;
+    
      packages.x86_64-linux.fleek = fleek.packages.x86_64-linux.default;
     
     # Available through 'home-manager --flake .#your-username@your-hostname'
@@ -69,6 +71,32 @@
           {
             home.packages = [
               fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
+      "qnm@macbook.local" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./macbook.local/qnm.nix
+          ./macbook.local/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.aarch64-darwin.default
             ];
           }
           ({
