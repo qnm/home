a9aa44370e5b902e12c728b8c78fdbf6ab113077
Rob Sharp <qnm@macbook.local>
2023-10-05 21:28:06 +1100

    fleek: new system

---
+++ macbook.local/qnm.nix
@@ -0,0 +1,29 @@
+{ pkgs, misc, ... }: {
+  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
+    home.username = "qnm";
+    home.homeDirectory = "/Users/qnm";
+    programs.git = {
+        enable = true;
+        aliases = {
+            pushall = "!git remote | xargs -L1 git push --all";
+            graph = "log --decorate --oneline --graph";
+            add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
+        };
+        userName = "Rob Sharp";
+        userEmail = "rob@sharp.id.au";
+        extraConfig = {
+            feature.manyFiles = true;
+            init.defaultBranch = "main";
+            gpg.format = "ssh";
+        };
+
+        signing = {
+            key = "";
+            signByDefault = builtins.stringLength "" > 0;
+        };
+
+        lfs.enable = true;
+        ignores = [ ".direnv" "result" ];
+  };
+  
+}
