4cd367331c79659caa091bc72f38fedafdc3a4de
Rob Sharp <rob@sharp.id.au>
2023-11-21 15:41:45 +1100

    flake update

--- home.nix
+++ home.nix
@@ -20,34 +20,6 @@
   # programs are installed and configuration applied to dotfiles
   home.packages = [
     # user selected packages
-    pkgs.shadowenv
-    pkgs.neofetch
-    pkgs.ripgrep
-    pkgs.curl
-    pkgs.unzip
-    pkgs.tmux
-    pkgs.terraform
-    pkgs.terraformer
-    pkgs.awscli2
-    pkgs.ssm-session-manager-plugin
-    pkgs.asdf-vm
-    pkgs.adrgen
-    pkgs.hasura-cli
-    pkgs.google-cloud-sdk
-    pkgs.copilot-cli
-    pkgs.yadm
-    pkgs.jq
-    pkgs.wget
-    pkgs.gnupg
-    pkgs.alacritty
-    pkgs.yadm
-    pkgs.graphviz
-    pkgs.dconf2nix
-    pkgs.ruby_3_2
-    pkgs.devbox
-    pkgs._1password-gui
-    pkgs.docker
-    pkgs.fish
     # Fleek Bling
     pkgs.git
     pkgs.htop
