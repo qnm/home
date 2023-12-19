9818f2b2ad2bd8de8905f6e94597efc9b9454dad
Rob Sharp <rob@sharp.id.au>
2023-12-20 09:07:30 +1100

    flake update

--- fedora/custom.nix
+++ fedora/custom.nix
@@ -1,7 +1,11 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-  home.sessionVariables = {
-    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+  programs.bash = {
+    enable = true;
+    profileExtra = ''
+      export XDG_DATA_DIRS=$HOME/.home-manager-share:$XDG_DATA_DIRS;
+      export DOCKER_HOST = unix://$XDG_RUNTIME_DIR/podman/podman.sock;
+    '';
   };
 }
