cd2adca16daf9e5a91ca822b312e4c7d9fe495df
Rob Sharp <rob@sharp.id.au>
2023-12-27 21:11:10 +1100

    join new system

--- flake.nix
+++ flake.nix
@@ -80,6 +80,32 @@
         ];
       };
       
+      "qnm@windoze" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./windoze/qnm.nix
+          ./windoze/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
     };
   };
 }
