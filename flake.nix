2384b6e245ef06131d184a5979774f0dfb2a7193
Rob Sharp <rob@sharp.id.au>
2024-06-24 11:22:27 +1000

    Remove fleek

--- flake.nix
+++ flake.nix
@@ -1,33 +1,21 @@
 {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
-  description = "Fleek Configuration";
+  description = "Home Manager Configuration";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
 
-    # Home manager
-    home-manager.url = "https://flakehub.com/f/nix-community/home-manager/0.1.tar.gz";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Fleek
-    fleek.url = "https://flakehub.com/f/ublue-os/fleek/*.tar.gz";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Overlays
-    
-
   };
 
-  outputs = { self, nixpkgs, home-manager, fleek, ... }@inputs: {
-    
-     packages.aarch64-darwin.fleek = fleek.packages.aarch64-darwin.default;
-    
-     packages.x86_64-linux.fleek = fleek.packages.x86_64-linux.default;
-    
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Available through 'home-manager --flake .#your-username@your-hostname'
-    
     homeConfigurations = {
-    
       "qnm@macbook.local" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
@@ -41,12 +29,6 @@
           # Host Specific configs
           ./macbook.local/qnm.nix
           ./macbook.local/custom.nix
-          # self-manage fleek
-          {
-            home.packages = [
-              fleek.packages.aarch64-darwin.default
-            ];
-          }
           ({
            nixpkgs.overlays = [];
           })
@@ -67,12 +49,6 @@
           # Host Specific configs
           ./fedora/qnm.nix
           ./fedora/custom.nix
-          # self-manage fleek
-          {
-            home.packages = [
-              fleek.packages.x86_64-linux.default
-            ];
-          }
           ({
            nixpkgs.overlays = [];
           })
@@ -93,12 +69,6 @@
           # Host Specific configs
           ./windoze/qnm.nix
           ./windoze/custom.nix
-          # self-manage fleek
-          {
-            home.packages = [
-              fleek.packages.x86_64-linux.default
-            ];
-          }
           ({
            nixpkgs.overlays = [];
           })
@@ -119,19 +89,12 @@
           # Host Specific configs
           ./robBook.local/qnm.nix
           ./robBook.local/custom.nix
-          # self-manage fleek
-          {
-            home.packages = [
-              fleek.packages.aarch64-darwin.default
-            ];
-          }
           ({
            nixpkgs.overlays = [];
           })
 
         ];
       };
-      
     };
   };
 }
--- home.nix
+++ home.nix
@@ -1,20 +1,15 @@
 { config, pkgs, misc, ... }: {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
   nixpkgs = {
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
-      
       allowUnfree = true;
+
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
-      
-      
     };
   };
 
-  
-  # managed by fleek, modify ~/.fleek.yml to change installed packages
   
   # packages are just installed (no configuration applied)
   # programs are installed and configuration applied to dotfiles
