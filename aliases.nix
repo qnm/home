54fe6c7548dac43b43fbd3a955629dac6eb73db4
Rob Sharp <rob@sharp.id.au>
2024-07-28 13:07:44 +1000

    Add reference to hack

--- aliases.nix
+++ aliases.nix
@@ -2,6 +2,7 @@
    home.shellAliases = {
     "fast.ai" = "docker run --gpus all -it --rm -p 8888:8888 -v .:/home/jovyan/work egineering/fastai:fastai-basic-dev-latest";
     "hm-switch" = "home-manager switch -b backup --flake . #qnm";
+    # https://github.com/ollama/ollama/issues/2934
     "unload-nvidia" = "sudo rmmod nvidia_uvm && sudo modprobe nvidia_uvm";
     };
 }
