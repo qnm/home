09966be3e6a7d98477fe5376fdbc44a4d23650d4
Rob Sharp <rob@sharp.id.au>
2024-11-05 09:22:11 +1100

    Small refactor

--- flake.nix
+++ flake.nix
@@ -2,25 +2,27 @@
   description = "Home Manager Configuration";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-24.05";
+    };
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    # Overlays
-    nixGL.url = "github:nix-community/nixGL";
-    nixGL.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+     url = "github:LnL7/nix-darwin";
+     inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nix-darwin, nixpkgs, nixGL, home-manager, ... }@inputs: {
-    # defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    # defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
-
-    # Available through 'home-manager --flake .#your-username@your-hostname'
+  outputs = { self, nix-darwin, nixpkgs, nixgl, home-manager, ... }@inputs: {
     darwinConfigurations = {
       "macbookpro" = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
@@ -49,68 +51,19 @@
     };
 
     homeConfigurations = {
-      "qnm@pop-os" = home-manager.lib.homeManagerConfiguration {
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-	    pkgs = import nixpkgs { system = "x86_64-linux"; allowUnfree = true; };
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+      "qnm@penguin" = home-manager.lib.homeManagerConfiguration ({
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          allowUnfree = true;
+          overlays = [ nixgl.overlay ];
+        };
         modules = [
-          ./home.nix
-          ./work.nix
-          ./path.nix
-          ./shell.nix
-          ./user.nix
-          ./aliases.nix
-          ./programs.nix
-          # Host Specific configs
-          ./fedora/qnm.nix
-          ./fedora/custom.nix
-          ({
-           nixpkgs.overlays = [ nixGL.overlay ];
+          ({ pkgs, ...}: {
+            # home.packages = with pkgs; [ nixgl.nixglMesa ];
           })
-
-        ];
-      };
-
-      "qnm@windoze" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        modules = [
           ./home.nix
-          ./path.nix
-          ./shell.nix
-          ./user.nix
-          ./aliases.nix
-          ./programs.nix
-          # Host Specific configs
-          ./windoze/qnm.nix
-          ./windoze/custom.nix
-          ({
-           nixpkgs.overlays = [];
-          })
-
         ];
-      };
-
-      "qnm@robBook.local" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-        modules = [
-          ./home.nix
-          ./path.nix
-          ./shell.nix
-          ./user.nix
-          ./aliases.nix
-          ./programs.nix
-          # Host Specific configs
-          ./robBook.local/qnm.nix
-          ./robBook.local/custom.nix
-          ({
-           nixpkgs.overlays = [];
-          })
-
-        ];
-      };
-
+      });
     };
   };
 }
