38fcbe87aa023aabf7aae5b21ae6833338024d48
Rob Sharp <rob@sharp.id.au>
2024-11-05 21:09:26 +1100

    Set up xdg on penguin

--- flake.nix
+++ flake.nix
@@ -59,10 +59,15 @@
             nixGL.packages = nixgl.packages;
             nixGL.defaultWrapper = "mesa";
             nixGL.installScripts = [ "mesa" ];
+
+            xdg.configFile."systemd/user/cros-garcon.service.d/override.conf".text = ''
+              [Service]
+              Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
+              Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
+            '';
           })
           ./home.nix
         ];
-
         extraSpecialArgs = {
           inherit nixgl;
         };
