457ee24630c1fb0e53b8a215e93b81acc837254e
Rob Sharp <rob@sharp.id.au>
2024-11-07 18:44:24 +1100

    Add zed to home manager

--- programs.nix
+++ programs.nix
@@ -3,4 +3,70 @@
     programs.direnv.nix-direnv.enable = true;
     programs.dircolors.enable = true;
     programs.gh.enable = true;
+
+    programs.zed-editor = {
+        enable = true;
+        extensions = [];
+        userKeymaps = with builtins; fromJSON ''
+            [
+                {
+                    "context": "Workspace",
+                    "bindings": {}
+                },
+                {
+                    "context": "Editor",
+                    "bindings": {
+                        "ctrl-k": "copilot::Suggest",
+                        "ctrl-shift-k": "copilot::NextSuggestion",
+                        "ctrl-j": "editor::AcceptInlineCompletion"
+                    }
+                }
+            ]
+        '';
+        userSettings = with builtins; fromJSON ''
+            {
+                "editor.codeActionsOnSave": {
+                    "source.fixAll.eslint": true
+                },
+                "features": {
+                    "inline_completion_provider": "copilot"
+                },
+                "assistant": {
+                        "default_model": {
+                        "provider": "ollama",
+                        "model": "llama3.2:1b-instruct-q5_0"
+                    },
+                    "version": "2"
+                },
+                "jupyter.enabled": true,
+                "vim_mode": true,
+                "ui_font_size": 16,
+                "buffer_font_size": 16,
+                "theme": {
+                    "mode": "system",
+                    "light": "One Light",
+                    "dark": "Catppuccin Mocha"
+                },
+                "language_models": {
+                    "ollama": {
+                        "api_url": "http://localhost:11434"
+                    }
+                },
+                "terminal": {
+                    "font_family": "Menlo"
+                },
+                "lsp": {
+                    "vtsls": {
+                        "settings": {
+                            "typescript": {
+                                "preferences": {
+                                    "importModuleSpecifier": "relative"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        '';
+    };
 }
