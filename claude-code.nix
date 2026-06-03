6026eb584a785585fc7ab06c9819bd6550243582
Robin Sharp <robin.sharp@amber.com.au>
2026-06-03 20:34:51 +1000

    sandbox and skills for claude

--- claude-code.nix
+++ claude-code.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 {
+  home.file.".claude/skills/hegelian-dialectic-skill/SKILL.md".source =
+    pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/KyleAMathews/hegelian-dialectic-skill/refs/heads/main/SKILL.md";
+      hash = "sha256-hhAKJTTg5D+UpTETunCmYj54X5qx9ERu2gZXZ1UcPN0=";
+    };
+
   programs.claude-code = {
     enable = true;
     package = pkgs.claude-code;
@@ -10,6 +16,16 @@
       };
       env = {
         CLAUDE_CODE_EXPERIMENTAL_AGENT_TEAMS = "1";
+      };
+      sandbox = {
+        enabled = true;
+        excludedCommands = [
+          "op *"
+          "gh *"
+        ];
+        network = {
+          allowMachLookup = [ "com.apple.trustd.agent" ];
+        };
       };
     };
   };
